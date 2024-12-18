044d8b0ed092bc2dd187af325515f566d012280a
Eric Alfred <alfrederic371@gmail.com>
2024-12-19 00:09:14 +0100

    update: models for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('OWNER', 'Property Owner'),
+        ('AGENT', 'Real Estate Agent'),
+        ('BUYER', 'Buyer/Renter'),
+    )
+    
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    phone = models.CharField(max_length=11, blank=True)
+    is_verified = models.BooleanField(default=False)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    
+    def __str__(self):
+        return self.username

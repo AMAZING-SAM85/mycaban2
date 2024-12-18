e717774608f06fd4aa8f4364387ebd5fc9f37ea7
Eric Alfred <alfrederic371@gmail.com>
2024-12-19 00:11:05 +0100

    chore: general urls

---
+++ properties/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import PropertyViewSet
+
+router = DefaultRouter()
+router.register(r'properties', PropertyViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]

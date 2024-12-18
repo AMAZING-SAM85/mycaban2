9ed129b20b7dceed227901d73a8f97e1159a6c63
Eric Alfred <alfrederic371@gmail.com>
2024-12-19 00:11:36 +0100

    feat: done with views for properties

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Property
+from .serializers import PropertySerializer
+from .filters import PropertyFilter
 
-# Create your views here.
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = PropertyFilter
+    search_fields = ['title', 'description', 'location']
+    ordering_fields = ['price', 'created_at']
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+

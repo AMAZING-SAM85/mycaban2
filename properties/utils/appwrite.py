1461f73c40466decd77c47d0c1757b21c494e273
Eric Alfred <alfrederic371@gmail.com>
2024-12-19 01:44:59 +0100

    feat: appwrite config successful

--- properties/utils/appwrite.py
+++ properties/utils/appwrite.py
@@ -4,15 +4,19 @@ from appwrite.client import Client
 from appwrite.services.storage import Storage
 from appwrite.input_file import InputFile
 from appwrite.id import ID
-from django.conf import settings
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class AppwriteHelper:
     def __init__(self):
         self.client = Client()
-        self.client.set_endpoint(settings.APPWRITE_ENDPOINT)
-        self.client.set_project(settings.APPWRITE_PROJECT_ID)
-        self.client.set_key(settings.APPWRITE_API_KEY)
+        self.client.set_endpoint(os.getenv('APPWRITE_ENDPOINT'))
+        self.client.set_project(os.getenv('APPWRITE_PROJECT_ID'))
+        self.client.set_key(os.getenv('APPWRITE_API_KEY'))
         self.storage = Storage(self.client)
+        print(f"{os.getenv('APPWRITE_ENDPOINT')} {os.getenv('APPWRITE_BUCKET_ID')} {os.getenv('APPWRITE_API_KEY')} {os.getenv('APPWRITE_PROJECT_ID')}")
     
     def upload_file(self, file, user_id):
         with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.name)[1]) as temp_file:
@@ -23,12 +27,15 @@ class AppwriteHelper:
         try:
             input_file = InputFile.from_path(temp_file_path)
             result = self.storage.create_file(
-                bucket_id=settings.APPWRITE_BUCKET_ID,
+                bucket_id=os.getenv('APPWRITE_BUCKET_ID'),
                 file_id=ID.unique(),
                 file=input_file
             )
+
+            print("File uploaded successfully:", result)
+            print(f"{os.getenv('APPWRITE_ENDPOINT')} {os.getenv('APPWRITE_BUCKET_ID')} {os.getenv('APPWRITE_API_KEY')} {os.getenv('APPWRITE_PROJECT_ID')}")
             
-            file_url = f"{settings.APPWRITE_ENDPOINT}/storage/buckets/{settings.APPWRITE_BUCKET_ID}/files/{result['$id']}/view?project={settings.APPWRITE_PROJECT_ID}"
+            file_url = f"{os.getenv('APPWRITE_ENDPOINT')}/storage/buckets/{os.getenv('APPWRITE_BUCKET_ID')}/files/{result['$id']}/view?project={os.getenv('APPWRITE_PROJECT_ID')}"
             return file_url
             
         finally:
